8f095fc90f2aee1181ccbb51572a604d3c1af0f4
luxinru <865666015@qq.com>
2023-06-17 17:38:37 +0800

    fixed bug

--- src/views/share.vue
+++ src/views/share.vue
@@ -114,7 +114,7 @@ export default {
         // {
         //   isVideo: false,
         //   name: "",
-        //   src: "http://juxingtuan.com/resources/imgs/img2.png",
+        //   src: "http://juxingtuan.com/resources/imgs/img2.jpg",
         // },
         // {
         //   isVideo: false,
